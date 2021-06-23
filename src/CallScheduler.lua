ce998b84c9e8c64ca4d3809b5a224593edea4429
GavinW <76234965+CozzyBro2@users.noreply.github.com>
2021-06-22 23:13:03 -0400

    Create CallScheduler.lua

---
+++ src/CallScheduler.lua
@@ -0,0 +1,30 @@
+local Scheduled = {} -- { Info = os.clock }
+local Scheduler = {Jitter = 0.005}
+
+game:GetService("RunService").Heartbeat:Connect(function()
+	for Call, Info in pairs(Scheduled) do
+		if os.clock() < Info.Date then continue end
+
+		Scheduler.Remove(Call)
+
+		Call(unpack(Info.Args))
+	end
+end)
+
+function Scheduler.Add(Time, Callback, ...)
+	assert(Time, "Invalid argument #1 'Time'"); assert(Callback, "Invalid argument #2 'Callback'")
+	
+	local Info = {
+		Date = (os.clock() + Time) - Scheduler.Jitter,
+		Call = Callback, 
+		Args = {...}
+	}
+
+	Scheduled[Callback] = Info
+end
+
+function Scheduler.Remove(Callback)
+	Scheduled[Callback] = nil
+end
+
+return Scheduler
